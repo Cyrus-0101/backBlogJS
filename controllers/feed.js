5e1131d944c0868174cc0262bf83ece5c0ded5a7
Toxic254 <cyrus_g@outlook.com>
2020-02-13 08:33:19 +0300

    First API practice with Postman testing endpoints and it worked. First Commit.

---
+++ controllers/feed.js
@@ -0,0 +1,16 @@
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(title, content);
+    //Create post in Db
+    res.status(201).json({
+        message: 'Post created Successfully',
+        post: {id: new Date().toISOString(), title: title, content: content  }
+    });
+};
---
+++ routes/feed.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const feedController = require('../controllers/feed');
+
+//GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+//POST /feed/posts
+router.post('/post', feedController.createPost)
+
+module.exports = router;
